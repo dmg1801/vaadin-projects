6cd7cc247cfb2343b799d4936cbe7c49415c945e
dmg1801 <mongaydiego@gmail.com>
2022-04-08 11:08:37 +0200

    grid with form at right side complete

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,5 +1,7 @@
 package com.example.application.views.list;
 
+import java.util.Collections;
+
 import com.example.application.data.entity.Contact;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
@@ -21,16 +23,37 @@ import com.vaadin.flow.router.Route;
 public class ListView extends VerticalLayout {
 	Grid<Contact> grid = new Grid<>(Contact.class);
 	TextField filterText = new TextField();
+	ContactForm form;
 
     public ListView() {
     	addClassName("list-view");
     	setSizeFull();
     	
     	configureGrid();
+    	configureForm();
     	
-    	add(getToolbar(),grid);
+    	add(
+    		getToolbar(),
+    		getContent()
+    	);
        
     }
+
+	private Component getContent() {
+		HorizontalLayout content = new HorizontalLayout(grid, form); 
+		content.setFlexGrow(2, grid);
+		content.setFlexGrow(1, form);
+		content.addClassName("content");
+		content.setSizeFull();
+		
+		return content;
+	}
+
+	private void configureForm() {
+		form = new ContactForm(Collections.emptyList(), Collections.emptyList());
+		form.setWidth("25em");
+		
+	}
 
 	private Component getToolbar() {
 		filterText.setPlaceholder("Filter by name...");
