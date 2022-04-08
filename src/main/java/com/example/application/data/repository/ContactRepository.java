8fde566e5d283f168f74197d13a0370d94d96f82
dmg1801 <mongaydiego@gmail.com>
2022-04-08 16:30:52 +0200

    error fixed on search term

--- src/main/java/com/example/application/data/repository/ContactRepository.java
+++ src/main/java/com/example/application/data/repository/ContactRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 public interface ContactRepository extends JpaRepository<Contact, UUID> {
 	
 	@Query("select c from Contact c " + 
-			"where lower(c.firstName) like lower(contact('%', :searchTerm, '%'))" +
+			"where lower(c.firstName) like lower(concat('%', :searchTerm, '%'))" +
 			"or lower(c.lastName) like lower(concat('%', :searchTerm, '%'))")
 	List<Contact> search(@Param("searchTerm") String searchTerm);
 
