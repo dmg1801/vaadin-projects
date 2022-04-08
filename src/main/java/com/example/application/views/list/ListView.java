c2fad05d636f835c3029ec5bc7af419f95f9de6a
dmg1801 <mongaydiego@gmail.com>
2022-04-08 09:18:33 +0200

    show message button exercise

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,9 +1,14 @@
 package com.example.application.views.list;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -12,19 +17,15 @@ import com.vaadin.flow.router.Route;
 public class ListView extends VerticalLayout {
 
     public ListView() {
-        setSpacing(false);
-
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+       Button button = new Button("Click me");
+       TextField name = new TextField("Name");
+       
+       HorizontalLayout hl = new HorizontalLayout(name, button);
+       hl.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+       
+       button.addClickListener(click-> Notification.show("Hello, " + name.getValue()));
+    		   
+       add(hl);
     }
 
 }
